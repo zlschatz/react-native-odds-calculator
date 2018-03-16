5df3f6813cf982dc629bfd2e7c99a07faa017b61
Zach <zachschatz@gmail.com>
2018-03-16 14:11:53 -0400

    Create empty Calculator Class

---
+++ Calculator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class Calculator extends Component<Props> {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
