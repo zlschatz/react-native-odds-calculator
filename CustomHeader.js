601fabb11a91fc04ae6d23e9936390f082652f1a
Zach <zachschatz@gmail.com>
2018-03-20 20:45:35 -0400

    Create custom header component

---
+++ CustomHeader.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Platform,
+  StyleSheet,
+  View
+} from 'react-native';
+import { Header, Left, Right, Button, Icon, Body, Title } from 'native-base';
+
+export default class CustomHeader extends Component {
+  render() {
+    return (
+      <View>
+      
+        <Header>
+          <Left>
+          </Left>
+          <Body>
+            <Title>OBW</Title>
+          </Body>
+          <Right>
+          </Right>
+        </Header>
+
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('CustomHeader', () => CustomHeader);
